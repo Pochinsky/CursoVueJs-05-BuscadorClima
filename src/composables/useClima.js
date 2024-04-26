00087a048aee8e70280abacd6ca96cc88966bf62
VicenteSina98 <133327651+VicenteSina98@users.noreply.github.com>
2024-04-25 21:27:56 -0400

    👽️ change http tp https url

--- src/composables/useClima.js
+++ src/composables/useClima.js
@@ -13,7 +13,7 @@ export default function useClima() {
     error.value = "";
     try {
       // obtener la latitud y la longitud
-      const url = `http://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=${key}`;
+      const url = `https://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=${key}`;
       const { data } = await axios(url);
       const { lat, lon } = data[0];
       //obtener el clima
